608e7928e371864a09272bdb4bdff4518091bb31
nardaiv <92304461+nardaiv@users.noreply.github.com>
2022-06-23 14:45:46 +0700

    manambahkan item pada slider

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 let i=0;
+let index=5
 
 document.getElementsByClassName("slide")[1].style.display = "none";
 document.getElementsByClassName("slide")[2].style.display = "none";
+document.getElementsByClassName("slide")[3].style.display = "none";
+document.getElementsByClassName("slide")[4].style.display = "none";
+
 
 function slider(arr){
     document.getElementsByClassName("slide")[i].style.display = "none";
@@ -10,6 +14,6 @@ function slider(arr){
     }else{
         i--;
     }
-    i= i>=3 ? 0: i<0? 2:i;
+    i= i>=index ? 0: i<0? index:i;
     document.getElementsByClassName("slide")[i].style.display = "block";
 }
