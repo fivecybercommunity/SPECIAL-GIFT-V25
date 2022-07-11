a45782a8a35b212c0ea59137f3ba79e337a19fae
aidanismail <aidanismail@yahoo.com>
2022-07-11 23:15:00 +0700

    finished

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
-let i=0;
-let index=5
+let i = 0;
+let index = 5;
 
 document.getElementsByClassName("slide")[1].style.display = "none";
 document.getElementsByClassName("slide")[2].style.display = "none";
 document.getElementsByClassName("slide")[3].style.display = "none";
 document.getElementsByClassName("slide")[4].style.display = "none";
 
-
-function slider(arr){
-    document.getElementsByClassName("slide")[i].style.display = "none";
-    if(arr==="next"){
-        i++;
-    }else{
-        i--;
-    }
-    i= i>=index ? 0: i<0? index:i;
-    document.getElementsByClassName("slide")[i].style.display = "block";
+function slider(arr) {
+  document.getElementsByClassName("slide")[i].style.display = "none";
+  if (arr === "next") {
+    i++;
+  } else {
+    i--;
+  }
+  i = i >= index ? 0 : i < 0 ? index : i;
+  document.getElementsByClassName("slide")[i].style.display = "block";
 }
